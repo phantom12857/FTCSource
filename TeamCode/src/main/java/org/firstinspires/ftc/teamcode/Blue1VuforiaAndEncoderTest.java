00f38b896382cc34c463f3d9aed49917ea76f323
WoodlandsRobotics <31572467+phantom12857@users.noreply.github.com>
2017-12-13 20:47:48 -0600

    uses encoders (method is in the config)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Blue1VuforiaAndEncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Blue1VuforiaAndEncoderTest.java
@@ -1,31 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 
 package org.firstinspires.ftc.teamcode;
 
@@ -47,8 +19,6 @@ public class Blue1VuforiaAndEncoderTest extends LinearOpMode {
 
     public static final String TAG = "Vuforia VuMark Sample";
 
-    OpenGLMatrix lastLocation = null;
-
     VuforiaLocalizer vuforia;
 
     /* Declare OpMode members. */
@@ -62,17 +32,11 @@ public class Blue1VuforiaAndEncoderTest extends LinearOpMode {
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
                                                                   (WHEEL_DIAMETER_INCHES * 3.1415);
 
-    
-    static final double     FORWARD_SPEED = 0.6;
-    static final double     TURN_SPEED    = 0.5;
 
     @Override
 
     public void runOpMode() {
-
-        //SET MODE TO BRAKE ON STOP
-        //robot.leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        //robot.rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        robot.runEncoder();
 
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
@@ -95,19 +59,15 @@ public class Blue1VuforiaAndEncoderTest extends LinearOpMode {
 
         robot.leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        robot.rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        robot.leftMotor.getCurrentPosition();
-        robot.rightMotor.getCurrentPosition();
         waitForStart();
 
         relicTrackables.activate();
-        int keyColumn = 0;
+        int keyDistance = 0;
 
 
         runtime.reset();
-        while (opModeIsActive() && runtime.seconds() < 5 && keyColumn == 0) {
+        while (opModeIsActive() && runtime.seconds() < 5 && keyDistance == 0) {
             RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
             if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
                 telemetry.addData("VuMark", "%s visible", vuMark);
@@ -115,36 +75,35 @@ public class Blue1VuforiaAndEncoderTest extends LinearOpMode {
             }
             if (vuMark == RelicRecoveryVuMark.LEFT) {
 
-                keyColumn = 1;
+                //DISTANCE FOR LEFT COLUMN
+                keyDistance = 30;
 
             } else if (vuMark == RelicRecoveryVuMark.CENTER) {
 
-                keyColumn = 2;
+                //DISTANCE FOR CENTER COLUMN
+                keyDistance = 40;
 
             } else if (vuMark == RelicRecoveryVuMark.RIGHT) {
 
-                keyColumn = 3;
+                //DISTANCE FOR RIGHT COLUMN
+                keyDistance = 50;
 
             }
 
         }
 
-        final int columnWidth = 24;
-        final int baseDistance = 36;
-        final int mainDistance = baseDistance + columnWidth * keyColumn;
-
         //STRAIGHT (distance with key column)
-        encoderDrive(FORWARD_SPEED, mainDistance, mainDistance, 0.5);
-        telemetry.addData("Forward", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+        robot.encoderDrive(robot.FORWARD_SPEED, keyDistance, keyDistance, 0);
+        telemetry.addData("Forward", keyDistance);
         telemetry.update();
 
         //left
-        encoderDrive(FORWARD_SPEED, -12, 12, 0.5);
+        robot.encoderDrive(robot.TURN_SPEED, -12, 12, 0);
         telemetry.addData("Right Turn", "Leg 2: %2.5f S Elapsed", runtime.seconds());
         telemetry.update();
 
         //straight
-        encoderDrive(FORWARD_SPEED, 24, 24, 0.5);
+        robot.encoderDrive(robot.FORWARD_SPEED, 24, 24, 0);
         telemetry.addData("Forward", "Leg 4: %2.5f S Elapsed", runtime.seconds());
         telemetry.update();
 
@@ -158,65 +117,11 @@ public class Blue1VuforiaAndEncoderTest extends LinearOpMode {
         }
 
         //stop
-        robot.leftMotor.setPower(0);
-        robot.rightMotor.setPower(0);
+        robot.stopRobot();
         robot.leftClaw.setPosition(0.0);
         robot.rightClaw.setPosition(0.0);
         telemetry.addData("In Safe Zone", "Complete");
         telemetry.update();
         sleep(1000);
-    }
-    public void encoderDrive(double speed,
-                             double leftInches, double rightInches,
-                             double timeoutS) {
-        int newLeftTarget;
-        int newRightTarget;
-
-        // Ensure that the opmode is still active
-        if (opModeIsActive()) {
-
-            // Determine new target position, and pass to motor controller
-            newLeftTarget = robot.leftMotor.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
-            newRightTarget = robot.rightMotor.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
-            robot.leftMotor.setTargetPosition(newLeftTarget);
-            robot.rightMotor.setTargetPosition(newRightTarget);
-
-            // Turn On RUN_TO_POSITION
-            robot.leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            // reset the timeout time and start motion.
-            runtime.reset();
-            robot.leftMotor.setPower(Math.abs(speed));
-            robot.rightMotor.setPower(Math.abs(speed));
-
-            // keep looping while we are still active, and there is time left, and both motors are running.
-            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
-            // its target position, the motion will stop.  This is "safer" in the event that the robot will
-            // always end the motion as soon as possible.
-            // However, if you require that BOTH motors have finished their moves before the robot continues
-            // onto the next step, use (isBusy() || isBusy()) in the loop test.
-            while (opModeIsActive() &&
-                    (runtime.seconds() < timeoutS) &&
-                    (robot.leftMotor.isBusy() && robot.rightMotor.isBusy())) {
-
-                // Display it for the driver.
-                telemetry.addData("Path1",  "Running to %7d :%7d", newLeftTarget,  newRightTarget);
-                telemetry.addData("Path2",  "Running at %7d :%7d",
-                        robot.leftMotor.getCurrentPosition(),
-                        robot.rightMotor.getCurrentPosition());
-                telemetry.update();
-            }
-
-            // Stop all motion;
-            robot.leftMotor.setPower(0);
-            robot.rightMotor.setPower(0);
-
-            // Turn off RUN_TO_POSITION
-            robot.leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            robot.rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-            //  sleep(250);   // optional pause after each move
-        }
     }
 }
